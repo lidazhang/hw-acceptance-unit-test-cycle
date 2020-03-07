aa234776f9e8859ebd99ea0a3d3f336809c7104f
EC2 Default User <ec2-user@ip-172-31-37-238.us-east-2.compute.internal>
2020-03-07 07:27:18 +0000

    part 1

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -59,6 +59,26 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def similar
+    @movie = Movie.find(params[:id])
+    if @movie.director.nil? || @movie.director.empty? || @movie.director.blank?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    else
+      @movies = Movie.same_directors(@movie.director)
+    end
+  end
+  
+  def same_director
+    @movie = Movie.find(params[:id])
+    if @movie.director.blank? || @movie.director.nil? 
+      flash[:notice] = "'#{@movie.title}' does not have director info"
+      redirect_to movies_path
+    else
+      @movies = Movie.find_same_director(@movie.director)
+    end
   end
 
 end
